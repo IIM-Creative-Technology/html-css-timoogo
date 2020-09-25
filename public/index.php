d29f585e62764c0068344f05d11c9997cc98a248
= <=>
2020-09-25 16:24:49 +0200

    mobile first section Explain terminé

--- public/index.php
+++ public/index.php
@@ -3,11 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Redline - Un nième site de sondage !</title>
-    <meta name="description" content="Un site de sondage guidé par un fils rouge !">
-    <meta name="author" content="timogo">
+    <title>Redline - un nième site de sondage </title>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    
+    <header>
+       <h1>
+           <a href="index.php">
+               <img src="assets/img/logo.png" alt="logo"> 
+           </a>
+       </h1>
+        <nav>
+        <ul>
+           <li><a href="index.php">Home</a></li>
+           <li><a href="#">Sondages</a></li>
+           <li><a href="#">Contact</a></li>
+           <li><a href="#">Classement</a></li>
+        </ul>    
+        </nav>
+    </header>
+    <section Explain>
+        <img src="assets/img/Polygone.png" alt="triangle stylisé">
+
+        <div class="wirl">
+            <h2>Sous-titre</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque dolor placeat ratione laborum quidem ducimus repudiandae ipsum incidunt maiores molestiae, nam esse! Illo soluta nam rerum pariatur libero architecto eos! Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi velit cumque nesciunt corporis, dicta neque architecto laborum, aperiam ipsa, dignissimos illo numquam minima inventore voluptatum voluptatem vitae. Corrupti, voluptatum veritatis. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim porro pariatur ipsam cumque maxime dolorum sequi inventore libero voluptate aut quam vel mollitia accusamus, molestiae quis quia dolore, provident tempora.</p>
+            <div class="button-wrapper">
+                <a href="#">CTA</a>
+            </div>
+        </div>
+    </section>
 </body>
 </html>
---
+++ views/templates/head.php
@@ -0,0 +1,4 @@
+
+<body>
+    salut je suis buggé
+</body>
