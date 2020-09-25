2adb4e6882cbe0930cbacc411e87f192bf0bb6d8
= <=>
2020-09-25 12:57:36 +0200

    passage à php

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Redline - Un nième site de sondage !</title>
+    <meta name="description" content="Un site de sondage guidé par un fils rouge !">
+    <meta name="author" content="timogo">
+</head>
+<body>
+    
+</body>
+</html>
