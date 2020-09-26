7990a14f81ff327cf85488d773ee5248c01e1640
= <=>
2020-09-26 23:28:16 +0200

    correctif [Team,Trust], début footer

--- public/index.php
+++ public/index.php
@@ -1,25 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Redline - un nième site de sondage </title>
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
 </head>
+
 <body>
     <header>
-       <h1>
-           <a href="index.php">
-               <img src="assets/img/logo.png" alt="logo"> 
-           </a>
-       </h1>
+        <h1>
+            <a href="index.php">
+                <img src="assets/img/logo.png" alt="logo">
+            </a>
+        </h1>
         <nav>
-        <ul>
-           <li><a href="index.php">Home</a></li>
-           <li><a href="#">Sondages</a></li>
-           <li><a href="#">Contact</a></li>
-           <li><a href="views/ranking.php">Classement</a></li>
-        </ul>    
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">Sondages</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="views/ranking.php">Classement</a></li>
+            </ul>
         </nav>
     </header>
     <section Explain>
@@ -27,44 +30,50 @@
 
         <div class="wirl">
             <h2>Sous-titre</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque dolor placeat ratione laborum quidem ducimus repudiandae ipsum incidunt maiores molestiae, nam esse! Illo soluta nam rerum pariatur libero architecto eos! Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi velit cumque nesciunt corporis, dicta neque architecto laborum, aperiam ipsa, dignissimos illo numquam minima inventore voluptatum voluptatem vitae. Corrupti, voluptatum veritatis. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim porro pariatur ipsam cumque maxime dolorum sequi inventore libero voluptate aut quam vel mollitia accusamus, molestiae quis quia dolore, provident tempora.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque dolor placeat ratione laborum quidem
+                ducimus repudiandae ipsum incidunt maiores molestiae, nam esse! Illo soluta nam rerum pariatur libero
+                architecto eos! Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi velit cumque nesciunt
+                corporis, dicta neque architecto laborum, aperiam ipsa, dignissimos illo numquam minima inventore
+                voluptatum voluptatem vitae. Corrupti, voluptatum veritatis. Lorem ipsum dolor sit amet consectetur,
+                adipisicing elit. Enim porro pariatur ipsam cumque maxime dolorum sequi inventore libero voluptate aut
+                quam vel mollitia accusamus, molestiae quis quia dolore, provident tempora.</p>
             <div class="button-wrapper">
                 <a href="#">CTA</a>
             </div>
         </div>
     </section>
     <section Discover>
-      <div class="titleDiscover">
-        <h2> Nos solutions BullShit 
-            <span>Qu'en pensez-vous ?</span>
-        </h2>
-      </div>
-      <div class="solutionsContainer">
-          <div class="singleSolution">
-              <h3>BullShit Solution</h3>
-              <img src="assets/img/bullshit.jpg" alt="BullShit">
-              <p>
-                Transformez le feedback en actions concrètes pour améliorer l'expérience client
-              </p>
-              <a href="#">Découvrir BullShit</a>
-          </div>
-          <div class="singleSolution">
-              <h3>BullShit Solution</h3>
-              <img src="assets/img/bullshit.jpg" alt="BullShit">
-              <p>
-                Transformez le feedback en actions concrètes pour améliorer l'expérience client
-              </p>
-              <a href="#">Découvrir BullShit</a>
-          </div>
-          <div class="singleSolution">
-              <h3>BullShit Solution</h3>
-              <img src="assets/img/bullshit.jpg" alt="BullShit">
-              <p>
-                Transformez le feedback en actions concrètes pour améliorer l'expérience client
-              </p>
-              <a href="#">Découvrir BullShit</a>
-          </div>
-      </div>
+        <div class="titleDiscover">
+            <h2> Nos solutions BullShit
+                <span>Qu'en pensez-vous ?</span>
+            </h2>
+        </div>
+        <div class="solutionsContainer">
+            <div class="singleSolution">
+                <h3>BullShit Solution</h3>
+                <img src="assets/img/bullshit.jpg" alt="BullShit">
+                <p>
+                    Transformez le feedback en actions concrètes pour améliorer l'expérience client
+                </p>
+                <a href="#">Découvrir BullShit</a>
+            </div>
+            <div class="singleSolution">
+                <h3>BullShit Solution</h3>
+                <img src="assets/img/bullshit.jpg" alt="BullShit">
+                <p>
+                    Transformez le feedback en actions concrètes pour améliorer l'expérience client
+                </p>
+                <a href="#">Découvrir BullShit</a>
+            </div>
+            <div class="singleSolution">
+                <h3>BullShit Solution</h3>
+                <img src="assets/img/bullshit.jpg" alt="BullShit">
+                <p>
+                    Transformez le feedback en actions concrètes pour améliorer l'expérience client
+                </p>
+                <a href="#">Découvrir BullShit</a>
+            </div>
+        </div>
     </section>
     <section Number>
         <div class="quote">
@@ -72,22 +81,27 @@
                 "Trouvez les bons interlocuteurs"
             </h2>
             <p>
-                Obtenez du feedback dans le monde entier, à tout moment. Les <a href="#surveys">études de marché</a> vous permettent de connaître l’avis de clients potentiels et existants, de tester des concepts, d’évaluer la notoriété de votre marque, et bien plus encore.
+                Obtenez du feedback dans le monde entier, à tout moment. Les <a href="#surveys">études de marché</a>
+                vous permettent de connaître l’avis de clients potentiels et existants, de tester des concepts,
+                d’évaluer la notoriété de votre marque, et bien plus encore.
             </p>
             <img src="assets/img/exemple-etude-de-marche.jpg" alt="" id="surveys">
         </div>
         <div class="numbersContainer">
             <div class="aNumber">
                 <h3>1800%</h3>
-                <p>De <span> productivité </span>en + que nos concurrents. Nous avons calculés ce chiffre grâce à nos experts du marketing que vous pouvez retrouver <a href="#team">ici</a>. </p>
+                <p>De <span> productivité </span>en + que nos concurrents. Nous avons calculés ce chiffre grâce à nos
+                    experts du marketing que vous pouvez retrouver <a href="#team">ici</a>. </p>
             </div>
             <div class="aNumber">
                 <h3>65%</h3>
-                <p>des <span>milléniaux</span> interrogés placent les valeurs de l'entreprise au-dessus du salaire proposé au moment de choisir un employeur. </p>
+                <p>des <span>milléniaux</span> interrogés placent les valeurs de l'entreprise au-dessus du salaire
+                    proposé au moment de choisir un employeur. </p>
             </div>
             <div class="aNumber">
                 <h3>TEST A/B</h3>
-                <p>Gagnez du temps et de l’argent grâce aux <span>tests de concept</span>. Obtenez du feedback en temps réel sur vos idées de nouveaux produits, campagnes marketing et plus encore.</a>. </p>
+                <p>Gagnez du temps et de l’argent grâce aux <span>tests de concept</span>. Obtenez du feedback en temps
+                    réel sur vos idées de nouveaux produits, campagnes marketing et plus encore.</a>. </p>
             </div>
         </div>
     </section>
@@ -110,8 +124,146 @@
                 <img src="assets/img/doug3.jpg" alt="Doug">
                 <span class="name">Doug</span>
             </div>
-      
+
         </div>
     </section>
+    <section Trust>
+        <h2>Ils nous font confiance</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae, aliquam? Deserunt doloribus, sapiente autem
+            placeat libero veniam excepturi ut quos nam iusto sunt? In illo ducimus eum totam voluptate laudantium!</p>
+        <div class="trustContainer">
+            <div class="user">
+                <img src="assets/img/logo.png" alt="Client">
+                <span>Client</span>
+            </div>
+            <div class="user">
+                <img src="assets/img/logo.png" alt="Client">
+                <span>Client</span>
+            </div>
+            <div class="user">
+                <img src="assets/img/logo.png" alt="Client">
+                <span>Client</span>
+            </div>
+            <div class="user">
+                <img src="assets/img/logo.png" alt="Client">
+                <span>Client</span>
+            </div>
+            <div class="user">
+                <img src="assets/img/logo.png" alt="Client">
+                <span>Client</span>
+            </div>
+            <div class="user">
+                <img src="assets/img/logo.png" alt="Client">
+                <span>Client</span>
+            </div>
+            <div class="user">
+                <img src="assets/img/logo.png" alt="Client">
+                <span>Client</span>
+            </div>
+            <div class="user">
+                <img src="assets/img/logo.png" alt="Client">
+                <span>Client</span>
+            </div>
+            <div class="user">
+                <img src="assets/img/logo.png" alt="Client">
+                <span>Client</span>
+            </div>
+            <div class="user">
+                <img src="assets/img/logo.png" alt="Client">
+                <span>Client</span>
+            </div>
+            <div class="user">
+                <img src="assets/img/logo.png" alt="Client">
+                <span>Client</span>
+            </div>
+            <div class="user">
+                <img src="assets/img/logo.png" alt="Client">
+                <span>Client</span>
+            </div>
+            <div class="user">
+                <img src="assets/img/logo.png" alt="Client">
+                <span>Client</span>
+            </div>
+            <div class="user">
+                <img src="assets/img/logo.png" alt="Client">
+                <span>Client</span>
+            </div>
+            <div class="user">
+                <img src="assets/img/logo.png" alt="Client">
+                <span>Client</span>
+            </div>
+            <div class="user">
+                <img src="assets/img/logo.png" alt="Client">
+                <span>Client</span>
+            </div>
+            <div class="user">
+                <img src="assets/img/logo.png" alt="Client">
+                <span>Client</span>
+            </div>
+            <div class="user">
+                <img src="assets/img/logo.png" alt="Client">
+                <span>Client</span>
+            </div>
+            <div class="user">
+                <img src="assets/img/logo.png" alt="Client">
+                <span>Client</span>
+            </div>
+            <div class="user">
+                <img src="assets/img/logo.png" alt="Client">
+                <span>Client</span>
+            </div>
+            <div class="user">
+                <img src="assets/img/logo.png" alt="Client">
+                <span>Client</span>
+            </div>
+            <div class="user">
+                <img src="assets/img/logo.png" alt="Client">
+                <span>Client</span>
+            </div>
+            <div class="user">
+                <img src="assets/img/logo.png" alt="Client">
+                <span>Client</span>
+            </div>
+            <div class="user">
+                <img src="assets/img/logo.png" alt="Client">
+                <span>Client</span>
+            </div>
+            <div class="user">
+                <img src="assets/img/logo.png" alt="Client">
+                <span>Client</span>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <div class="social">
+            <hr>
+            <a href="#" class="social_link">
+                    <span><i class="fab fa-github"></i></span>
+                </div>
+                <div class="subTitleSocial">timoogo</div>
+            </a>
+        </div>
+        <div class="social">
+            <hr>
+            <a href="#" class="social_link">
+                    <span><i class="fab fa-github"></i></span>
+                </div>
+                <div class="subTitleSocial">timoogo</div>
+            </a>
+        </div>
+        <div class="social">
+            <hr>
+            <a href="#" class="social_link">
+                    <span><i class="fab fa-github"></i></span>
+                </div>
+                <div class="subTitleSocial">timoogo</div>
+            </a>
+        </div>
+        <div class="credits">
+            <p>Copyrights ©︎ 2020 timogo.</p>
+        </div>
+    </footer>
 </body>
+
 </html>
